5077526a218c0755a68ba5ed0eaa5c1d388e4ad2
Clare Mason <clarermason@yahoo.com>
2018-02-01 10:20:15 -0800

    Update submit page
    Changed up how HTML was generated to make styling easier

--- submit.php
+++ submit.php
@@ -2,14 +2,9 @@
 session_start();
 if(isset($_SESSION['total'])){
 
-$total = $_SESSION['total'];
-    echo "<h3>Thank you for your order!</h3>";
-    echo "Tax:$";
-    $tax = number_format((float)$total * .065, 2, '.', '');
-    echo $tax;
-    echo "<br>";
-    echo "Your total is:$";
-    echo number_format((float)$total + $tax, 2, '.', '');
+    $total = $_SESSION['total'];
+    $tax = number_format((float)$total * .065, 2, '.', '');    
+    $total = number_format((float)$total + $tax, 2, '.', '');
 
 
 }
@@ -17,3 +12,31 @@ $total = $_SESSION['total'];
 else{
     echo "You didn't order anything :(";
 }
+
+?>
+
+<html lang="en">
+	
+	<head>
+		<meta charset="UTF-8">
+		<title>Food Truck  -  Menu</title>
+		<link href="styles.css" type="text/css" rel="stylesheet" />		
+	</head>
+
+	<body>
+		<header>
+		</header>
+		
+		<main>
+			<h3> Thank You for your Order!</h3>
+<br>
+    Tax:$<?=$tax?>
+<br>
+Total:$ <?=$total?>
+<br>
+		</main>
+		<footer>
+		</footer>
+	</body>
+	
+</html>
